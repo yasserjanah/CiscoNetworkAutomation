1ea0333f2f917549ba4b8d950f6d5d37234e0bf6
Yasser Janah <janah.y4ss3r@gmail.com>
2019-10-04 18:09:59 +0100

    Create TelnetSession Class

--- CiscoNetworkAutomation.py
+++ CiscoNetworkAutomation.py
@@ -1,23 +1,42 @@
 #!/usr/bin/env python3
 
+__author__ = "Yasser Janah"
+__email___ = "janah.y4ss3r@gmail.com"
+
 try:
     import os
     import sys
+    import telnetlib
 except ImportError as err:
     exit(str(err))
 
 class msg:
-    def __init__(self, _msg):
-        self._img = _msg
+    def __init__(self):
+        self.W = "\e[39m"
 
-    def failure(self):
+    def failure(self, _msg):
         pass
 
-    def success(self):
+    def success(self, _msg):
         pass
 
-    def warning(self):
+    def warning(self, _msg):
         pass
 
-    def info(self):
+    def info(self, _msg):
         pass
+
+class TelnetSession:
+    def __init__(self, _host):
+        self._host = _host
+        self._tn = telnetlib.Telnet(self._host)
+
+    def run(self):
+        self._tn.read_until(b"Username: ")
+        self._tn.write("root".encode('ascii') + b"\n")
+        self._tn.read_until(b"Password: ")
+        self._tn.write("janah".encode('ascii') + b"\n")
+        print(self._tn.read_all())
+
+
+TelnetSession('localhost').run()
