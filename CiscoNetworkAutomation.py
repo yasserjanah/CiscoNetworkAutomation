e0126950ddfa89d19b19009269aa0738ad9d142d
Yasser Janah <janah.y4ss3r@gmail.com>
2019-10-05 01:08:46 +0100

    create msg class

--- CiscoNetworkAutomation.py
+++ CiscoNetworkAutomation.py
@@ -7,36 +7,61 @@ try:
     import os
     import sys
     import telnetlib
+    import time
 except ImportError as err:
     exit(str(err))
 
-class msg:
-    def __init__(self):
-        self.W = "\e[39m"
+class msg(object):
+    WHITE  = u"\u001b[38;5;255m"
+    RED    = u"\u001b[38;5;196m"
+    GREEN  = u"\u001b[38;5;40m"
+    BLUE   = u"\u001b[38;5;21m"
+    YELLOW = u"\u001b[38;5;220m"
+    
+    @staticmethod
+    def failure(_msg:str, very=False):
+        if very is True:
+            text = f"{msg.WHITE}[{msg.RED}CRITICAL{msg.WHITE}] {msg.RED}{_msg}{msg.WHITE}"
+        else:
+            text = f"{msg.WHITE}[{msg.RED}CRITICAL{msg.WHITE}] {_msg}{msg.WHITE}"
+        print(text)
 
-    def failure(self, _msg):
-        pass
+    @staticmethod
+    def success(_msg:str):
+        text = f"{msg.WHITE}[{msg.GREEN}SUCCESS{msg.WHITE}] {_msg}{msg.WHITE}"
+        print(text)
 
-    def success(self, _msg):
-        pass
+    @staticmethod
+    def warning(_msg:str):
+        text = f"{msg.WHITE}[{msg.YELLOW}WARNING{msg.WHITE}] {_msg}{msg.WHITE}"
+        print(text)
 
-    def warning(self, _msg):
-        pass
-
-    def info(self, _msg):
-        pass
+    @staticmethod
+    def info(_msg:str):
+        text = f"{msg.WHITE}[{msg.BLUE}INFO{msg.WHITE}] {_msg}{msg.WHITE}"
+        print(text)
 
 class TelnetSession:
-    def __init__(self, _host):
+    def __init__(self, _host:str, _port=23):
         self._host = _host
-        self._tn = telnetlib.Telnet(self._host)
+        self._port = _port
+        try:
+            self._tn = telnetlib.Telnet(self._host, self._port)
+            msg.success("Connection etablished !! ")
+        except Exception as err:
+            exit(msg.failure(err, True))
 
     def run(self):
+        msg.info('starting login .....')
         self._tn.read_until(b"Username: ")
         self._tn.write("root".encode('ascii') + b"\n")
+        time.sleep(1)
         self._tn.read_until(b"Password: ")
+        msg.info('writing password .....')
         self._tn.write("janah".encode('ascii') + b"\n")
-        print(self._tn.read_all())
+        time.sleep(1)
+        self._tn.write(b'exit\n')
+        print(self._tn.read_all().decode('ascii'))
 
 
-TelnetSession('localhost').run()
+TelnetSession('10.0.0.1').run()
